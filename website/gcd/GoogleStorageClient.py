8000910d14ed44f3a8a8f5400570be0ea9acf6db
Jiajun Dai <jdai10@mail.ccsf.edu>
2023-08-19 13:27:53 -0700

    added .env

--- website/gcd/GoogleStorageClient.py
+++ website/gcd/GoogleStorageClient.py
@@ -1,10 +1,12 @@
 from google.cloud import storage
+from dotenv import dotenv_values
 import os
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'C:/Users/jdai1/OneDrive/Desktop/PythonWorkSpace/AimiesGallery.flask/website/static/aimiefung-art-db-395704-97e3664dac9e.json'
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'C:/Users/jdai1/OneDrive/Desktop/PythonWorkSpace/AimiesGallery.flask/website/static/aimiefung-art-db-395704-97e3664dac9e.json'
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = dotenv_values(".env")
 
 
 # authenticate to Cloud Storage
 def get_google_authentication():
-    storage_client = storage.Client()
+    storage_client = storage.Client(project='aimiefung-art-db-395704')
     return storage_client
